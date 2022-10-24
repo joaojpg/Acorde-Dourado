1bcc2d72260ec4a51bfc14b728b26a95c8e629b0
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-10-24 20:51:02 -0300

    ChegaPorHoje

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,19 @@
+import viola from '../pages/violao.jpg'
+import styles from './Home.module.css'
+
 function Home() {
     return(
         <div>
-            <h1>Home</h1>
-            <p>Conteúdo da página</p>
+            <section className={styles.imagem}>
+                <section className={styles.fundo}>
+                    <h3>
+                        Qualidade e História
+                    </h3>
+                    <p>
+                        Nossos instrumentos são produzidos com carinho e dedicação, buscando trazer toda a nossa história como bagagem mas mantendo a modernidade das novas tecnologias
+                    </p>
+                </section>                
+            </section>
         </div>
     )
 }
