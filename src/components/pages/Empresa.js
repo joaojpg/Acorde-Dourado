b8bcf47fa5aa4db9c85607650c96ad52fda8cf9a
Vinicius Emanoel Mendes <mendes.vinicius7575@gmail.com>
2022-10-25 20:13:03 -0300

    ALterações

--- src/components/pages/Empresa.js
+++ src/components/pages/Empresa.js
@@ -1,8 +1,25 @@
+import styles from './Empresa.module.css'
+
+
 function Empresa() {
     return(
         <div>
-            <h1>Empresa</h1>
-            <p>Conteúdo da página</p>
+            <section className={styles.imagem}>
+                <section className={styles.fundo}>
+                    <h3 className={styles.boxText}>
+                        Nossa Empresa
+                    </h3>
+                    <p className={styles.boxText}>
+                        A Acorde Dourado nasceu em 1925 quando Joseph Anderson, aos 15 anos na época, se interessou pelos instrumentos musicais rústicos que existiam naquele tempo. Seu pai tinha uma loja de produtos caseiros localizada no centro de Campinas e Joseph sempre ajudava seu pai com as vendas. 
+                    </p>
+                    <p className={styles.boxText}>
+                        Olhando pra isso, e tambem para seu amor pelos instrumentos musicais, ele tomou a iniciativa de abrir um negócio próprio no qual os principais produtos seriam instrumentos caseiros. 
+                    </p>
+                    <p className={styles.boxText}>
+                        Desde então a empresa Acorde Dourado cresceu sem freios e hoje é uma das lojas mais consideradas no mercado de instrumentos musicais rusticos
+                    </p>
+                </section>                
+            </section>
         </div>
     )
 }
